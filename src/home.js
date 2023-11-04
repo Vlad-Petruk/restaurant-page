2472c94104231525c761deff17fca56a02d7752b
Vlad-Petruk <vladpetruk.00@gmail.com>
2023-11-04 14:22:06 +0200

    home page with js

--- src/home.js
+++ src/home.js
@@ -30,6 +30,27 @@ function loadMainPage() {
 
     // Body
     const body = document.createElement('div');
+    body.classList.add('body');
+
+    const mainInfo = document.createElement('div');
+    mainInfo.classList.add('main-info');
+    
+    const advertText = document.createElement('div');
+    advertText.classList.add('advert-text');
+    advertText.textContent = 'Wine cellar, wine tasting, 🍕wood-fired Neapolitan pizza 🍝Italian pasta 🥩🍤grilled or baked meat and fish, original sushi';
+
+    const advertPhoto = document.createElement('div');
+    advertPhoto.classList.add('advert-photo');
+    const chefPhoto = document.createElement('img');
+    chefPhoto.classList.add('chef');
+    chefPhoto.src= '../img/chef.jpeg'
+    advertPhoto.appendChild(chefPhoto);
+
+    mainInfo.appendChild(advertText)
+    mainInfo.appendChild(advertPhoto)
+
+    body.appendChild(mainInfo);
+    content.appendChild(body)
 }
 
 export default loadMainPage
